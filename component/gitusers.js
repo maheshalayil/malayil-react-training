f1875221964908c4e8da017a6abfa3d5dfea9dcb
maheshalayil <dotmahee@gmail.com>
2019-06-24 15:10:25 +0530

    1 file modified

--- component/gitusers.js
+++ component/gitusers.js
@@ -11,9 +11,7 @@ export default class GitUser extends  React.Component {
         <p>git users - {this.state.users.length}</p>
         <ul>
          {
-           this.state.users.map(x=> {
-             return <li>{x.login}</li>
-           })
+           this.state.users.map(x=> <li>{x.login}</li> )
          }
          </ul>
       </div>
